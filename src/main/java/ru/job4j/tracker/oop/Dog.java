6826866139068aed05410875fd16139e52d3bfad
Alex <aleksandr.litvinov89qw@gmail.com>
2025-02-12 19:17:02 +0700

    1.1. Создание объекта

--- src/main/java/ru/job4j/tracker/oop/Dog.java
+++ src/main/java/ru/job4j/tracker/oop/Dog.java
@@ -2,5 +2,7 @@ package ru.job4j.tracker.oop;
 
 public class Dog {
 
-
+    public void bark() {
+        System.out.println("Гав-гав");
+    }
 }
--- src/main/java/ru/job4j/tracker/oop/Main.java
+++ src/main/java/ru/job4j/tracker/oop/Main.java
@@ -2,8 +2,15 @@ package ru.job4j.tracker.oop;
 
 public class Main {
     public static void main(String[] args) {
+        /**
+         *  Создаем три объекта класса Dog
+         */
         Dog polkan = new Dog();
+        polkan.bark();
         Dog sharik = new Dog();
+        sharik.bark();
         Dog zhychka = new Dog();
+        zhychka.bark();
+        System.out.println("Созданы три объекта Dog.");
     }
 }
