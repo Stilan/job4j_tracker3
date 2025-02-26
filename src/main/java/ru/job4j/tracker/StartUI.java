26f341f06250898e3ffaf933694b905708067c5a
Alex <aleksandr.litvinov89qw@gmail.com>
2025-02-26 20:02:08 +0700

    7. Метод удаления заявки Tracker.delete

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,14 +1,23 @@
 package ru.job4j.tracker;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
 public class StartUI {
-
     public static void main(String[] args) {
         Item item = new Item(1, "Привет");
-        LocalDateTime localDateTime = item.getCreated();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-        System.out.println(localDateTime.format(formatter));
+        System.out.println(item);
     }
 }
+
+
+
+
+
+
+
+
+
+//    Item item = new Item(1, "Привет");
+//    LocalDateTime localDateTime = item.getCreated();
+//    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+//        System.out.println(localDateTime.format(formatter));
+//                User user = new User(1, "root", "root");
+//                System.out.println(user);
